ca1ed0afb48e08633957532e83cd5510d6f0cda7
Alessio2704 <marcuzzial@gmail.com>
2025-03-04 21:24:32 +0100

    initial commit

--- Sources/HapticManager/HapticManager.swift
+++ Sources/HapticManager/HapticManager.swift
@@ -1,2 +1,20 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import UIKit
+
+@MainActor
+public class HapticManager {
+    
+    public static let shared = HapticManager()
+    
+    public func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    public func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+}
